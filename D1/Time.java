71a7b5b7e7685ac13d59e172a8ee85d61039fa3c
rei905 <dexter.zurbruegg@tie-international.com>
2024-11-11 16:01:22 +0100

    Time Update

--- D1/Time.java
+++ D1/Time.java
@@ -1,10 +1,37 @@
 public class Time {
-    private int hour;
-    private int minute;
-    private int second;
+    private int hour = 0;
+    private int minute = 0;
+    private int second = 0;
 
     public Time(int hour, int minute, int second) {
         setTime(hour, minute, second);
+    }
+
+    public int getHour() { return hour; }
+    public int getMinute() { return minute; }
+    public int getSecond() { return second; }
+
+    public void int setHour(newHour) { hour = newHour; }
+    public void int setMinute(newMinute) { minute = newMinute; }
+    public void int setSecond(newSecond) { second = newSecond; }
+
+    public String toString() {
+        return String.format("%02d:%02d:%02d", hour, minute, second);
+    }
+
+    public void nextSecond() {
+        second++;
+        if (second == 60) {
+            second = 0;
+            minute++;
+            if (minute == 60) {
+                minute = 0;
+                hour++;
+                if (hour == 24) {
+                    hour = 0;
+                }
+            }
+        }
     }
 
     public void setTime(int hour, int minute, int second) {
@@ -20,28 +47,5 @@ public class Time {
         this.hour = hour;
         this.minute = minute;
         this.second = second;
-    }
-
-    public int getHour() { return hour; }
-    public int getMinute() { return minute; }
-    public int getSecond() { return second; }
-
-    public String toString() {
-        return String.format("%02d:%02d:%02d", hour, minute, second);
-    }
-
-    public void tick() {
-        second++;
-        if (second == 60) {
-            second = 0;
-            minute++;
-            if (minute == 60) {
-                minute = 0;
-                hour++;
-                if (hour == 24) {
-                    hour = 0;
-                }
-            }
-        }
     }
 }
