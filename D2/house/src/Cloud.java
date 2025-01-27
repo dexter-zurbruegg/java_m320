4fac6d087aa2c0edc2b24afd723eebb8414aee75
zxch2208 <zachary.jenkins@tie-international.com>
2025-01-27 13:33:39 +0100

    added javadocs

--- D2/house/src/Cloud.java
+++ D2/house/src/Cloud.java
@@ -3,9 +3,21 @@ package D2.house.src;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a cloud composed of multiple circles.
+ * The cloud is created to visually represent a cloud in a graphical application.
+ */
 public class Cloud {
     private List<Circle> circles; // multiple circles to represent the clouds
 
+    /**
+     * Constructs a cloud at a specified position with a given size.
+     * The cloud is made up of three circles to give it a realistic appearance.
+     *
+     * @param x the horizontal position of the cloud
+     * @param y the vertical position of the cloud
+     * @param size the size of each circle in the cloud
+     */
     public Cloud(int x, int y, int size) {
         circles = new ArrayList<>();
 
@@ -20,6 +32,13 @@ public class Cloud {
         }
     }
 
+    /**
+     * Moves the cloud horizontally by a specified distance.
+     * The movement is animated to create a smooth transition.
+     * If the cloud moves beyond a certain boundary, it wraps around to the other side.
+     *
+     * @param distance the distance to move the cloud; positive for right, negative for left
+     */
     public void move(int distance) {
         for (int i = 0; i < Math.abs(distance); i++) {
             for (Circle circle : circles) {
@@ -28,12 +47,6 @@ public class Cloud {
                 } else {
                     circle.slowMoveHorizontal(-1);
                 }
-            }
-        }
-
-        if (circles.get(0).getDiameter() + circles.get(0).getXPosition() > 500) {
-            for (Circle circle : circles) {
-                circle.moveHorizontal(-500);
             }
         }
     }
