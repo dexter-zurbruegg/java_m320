5e58efab901270fca315fd39d647281d60ae6109
rei905 <dexter.zurbruegg@tie-international.com>
2025-01-27 22:04:50 +0100

    book categories, ratings and popularity

--- Level4/src/Book.java
+++ Level4/src/Book.java
@@ -8,41 +8,39 @@ public class Book {
     private String ISBN;
     private boolean isAvailable;
     private Date dueDate;
+    private String category;
+    private double rating;
+    private int borrowCount;
 
-    public Book(String title, String author, String ISBN) {
+    public Book(String title, String author, String ISBN, String category) {
         this.title = title;
         this.author = author;
         this.ISBN = ISBN;
         this.isAvailable = true;
         this.dueDate = null;
+        this.category = category;
+        this.rating = 0.0;
+        this.borrowCount = 0;
     }
 
-    public String getTitle() {
-        return title;
+    // Getters and setters
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public String getISBN() { return ISBN; }
+    public boolean isAvailable() { return isAvailable; }
+    public void setAvailable(boolean available) { isAvailable = available; }
+    public Date getDueDate() { return dueDate; }
+    public void setDueDate(Date dueDate) { this.dueDate = dueDate; }
+    public String getCategory() { return category; }
+    public double getRating() { return rating; }
+    public int getBorrowCount() { return borrowCount; }
+
+    public void updateRating(double newRating) {
+        this.rating = (this.rating * borrowCount + newRating) / (borrowCount + 1);
     }
 
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getISBN() {
-        return ISBN;
-    }
-
-    public boolean isAvailable() {
-        return isAvailable;
-    }
-
-    public void setAvailable(boolean available) {
-        isAvailable = available;
-    }
-
-    public Date getDueDate() {
-        return dueDate;
-    }
-
-    public void setDueDate(Date dueDate) {
-        this.dueDate = dueDate;
+    public void incrementBorrowCount() {
+        this.borrowCount++;
     }
 
     public double calculateFine() {
@@ -53,7 +51,7 @@ public class Book {
         if (currentDate.after(dueDate)) {
             long diff = currentDate.getTime() - dueDate.getTime();
             long daysOverdue = diff / (24 * 60 * 60 * 1000);
-            return daysOverdue * 5; // $5 fine per day
+            return daysOverdue * 5;
         }
         return 0.0;
     }
