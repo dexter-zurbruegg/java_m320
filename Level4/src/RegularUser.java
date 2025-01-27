07d8f40298e11084e8aa9608a85d7f928e4aa0ee
rei905 <dexter.zurbruegg@tie-international.com>
2025-01-27 21:52:24 +0100

    paying fines for RegularUser

--- Level4/src/RegularUser.java
+++ Level4/src/RegularUser.java
@@ -5,11 +5,26 @@ public class RegularUser extends User {
         super(name, userId);
     }
 
+    @Override
     public void borrowBook(Book book) throws LibraryException {
         Library.getInstance().borrowBook(this, book);
+        super.borrowBook(book);
     }
 
+    @Override
     public void returnBook(Book book) {
+        double fine = book.calculateFine();
+        if (fine > 0) {
+            System.out.println("Fine to be paid: $" + fine);
+        }
         Library.getInstance().returnBook(this, book);
+        super.returnBook(book);
+    }
+
+    public void viewBorrowedBooks() {
+        System.out.println("Books borrowed by " + getName() + ":");
+        for (Book book : getBorrowedBooks()) {
+            System.out.println(book.getTitle() + " by " + book.getAuthor());
+        }
     }
 }
