8c324e76b4a8d097cf080e50840e37462bba9b0f
rei905 <dexter.zurbruegg@tie-international.com>
2025-01-27 11:10:07 +0100

    adding getter for x position of circle & logic to move circles simultaneously

--- D2/house/src/Cloud.java
+++ D2/house/src/Cloud.java
@@ -1,2 +1,40 @@
-package D2.house.src;public class Cloud {
+package D2.house.src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cloud {
+    private List<Circle> circles; // multiple circles to represent the clouds
+
+    public Cloud(int x, int y, int size) {
+        circles = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            Circle circle = new Circle();
+            circle.changeColor("gray");
+            circle.changeSize(size);
+            circle.moveHorizontal(x + (i * (size / 2)));
+            circle.moveVertical(y);
+            circle.makeVisible();
+            circles.add(circle);
+        }
+    }
+
+    public void move(int distance) {
+        for (int i = 0; i < Math.abs(distance); i++) {
+            for (Circle circle : circles) {
+                if (distance > 0) {
+                    circle.slowMoveHorizontal(1);
+                } else {
+                    circle.slowMoveHorizontal(-1);
+                }
+            }
+        }
+
+        if (circles.get(0).getDiameter() + circles.get(0).getXPosition() > 500) {
+            for (Circle circle : circles) {
+                circle.moveHorizontal(-500);
+            }
+        }
+    }
 }
