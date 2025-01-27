8f4c56e6f7b6e2cfd2d987f4d4c88a5640e10776
rei905 <dexter.zurbruegg@tie-international.com>
2025-01-27 21:33:26 +0100

    calculating due date of books and fine

--- Level4/src/Book.java
+++ Level4/src/Book.java
@@ -1,16 +1,20 @@
 package Level4.src;
 
+import java.util.Date;
+
 public class Book {
     private String title;
     private String author;
     private String ISBN;
     private boolean isAvailable;
+    private Date dueDate;
 
     public Book(String title, String author, String ISBN) {
         this.title = title;
         this.author = author;
         this.ISBN = ISBN;
         this.isAvailable = true;
+        this.dueDate = null;
     }
 
     public String getTitle() {
@@ -31,5 +35,26 @@ public class Book {
 
     public void setAvailable(boolean available) {
         isAvailable = available;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public double calculateFine() {
+        if (dueDate == null || isAvailable) {
+            return 0.0;
+        }
+        Date currentDate = new Date();
+        if (currentDate.after(dueDate)) {
+            long diff = currentDate.getTime() - dueDate.getTime();
+            long daysOverdue = diff / (24 * 60 * 60 * 1000);
+            return daysOverdue * 5; // $5 fine per day
+        }
+        return 0.0;
     }
 }
